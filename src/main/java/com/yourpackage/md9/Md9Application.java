74ad8c887cb50f8b991bc3e8c690a9074a51213d
Emad Silawi <emad.silawi89@gmail.com>
2024-10-21 16:15:57 +0300

    Adding Login and Admin Pages in FE, Setting up BE

---
+++ src/main/java/com/yourpackage/md9/Md9Application.java
@@ -0,0 +1,13 @@
+package com.yourpackage.md9;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Md9Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Md9Application.class, args);
+	}
+
+}
